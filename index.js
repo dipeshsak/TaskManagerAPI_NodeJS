6994da7820ee38f58202a307207db9189490b837
Dipesh Sakharkar <dipeshsakharkar1@gmail.com>
2023-07-03 23:16:39 +0530

    createTask getalltask & single task feature added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('./db/connect')
 const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
+const connectDB  = require('./db/connect')
+require('dotenv').config()
 
 const port = 3000
 
@@ -10,4 +13,14 @@ app.use(express.json())
 //routes
 app.use('/api/v1/tasks',tasks)
 
-app.listen(port,console.log(`Server is listening on port ${port}... `))
+const start =async()=>{
+    try{
+     await connectDB(process.env.MONGO_URI)
+     app.listen(port,console.log(`Server is listening on port ${port}... `))
+    }catch(error){
+   console.log(error)
+    }
+}
+
+start()
+
