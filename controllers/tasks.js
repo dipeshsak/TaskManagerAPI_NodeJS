cf152076155e63a18e25013dcfe214aa0f603adf
Dipesh Sakharkar <dipeshsakharkar1@gmail.com>
2023-07-03 23:30:07 +0530

    update and delete functionality added

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -31,14 +31,33 @@ try{
 }
 };
 
-const updateTask = (req, res) => {
-  let id = req.params.id;
-  res.json(id);
+const updateTask = async(req, res) => {
+  try{
+    const {id:taskID} = req.params
+    const task = await Task.findOneAndUpdate({_id:taskID},req.body,{
+      new:true,
+      runValidators:true
+    })
+    if(!task){
+      return res.status(404).json({msg:`No task with id : ${taskID}`})
+    }
+    res.status(200).json({task})
+  }catch(error){
+    res.status(500).json({msg:error})
+  }
 };
 
-const deleteTask = (req, res) => {
-  let id = req.params.id;
-  res.json(id);
+const deleteTask = async(req, res) => {
+  try{
+    const {id:taskID} = req.params
+    const task = await Task.findOneAndDelete({_id:taskID})
+    if(!task){
+      return res.status(404).json({msg:`No task with id : ${taskID}`})
+    }
+    res.status(200).json({deletedTask:task,status:'success'})
+  }catch(error){
+    res.status(500).json({msg:error})
+  }
 };
 
 module.exports = {
